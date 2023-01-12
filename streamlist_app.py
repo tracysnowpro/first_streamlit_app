cb081e59399b177abd7e77051004bf9cdfcf2574
tracysnowpro <122344839+tracysnowpro@users.noreply.github.com>
2023-01-12 13:31:15 +0800

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -27,7 +27,6 @@ def get_fruityvice_data(this_fruit_choice):
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
 
-
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -40,12 +39,10 @@ try:
 except URLError as e:
     sytreamlit.error()
     
-
-   
 def get_fruit_load_list():
    with my_cnx.cursor as my_cur:
       my_cur.execute("select * from fruit_load_list")
-      return my_cur.fetall()
+      return my_cur.fetchall()
 
 # Add a button to load the fruit
 if streamlit.buttin('Get Fuit Load List'):
