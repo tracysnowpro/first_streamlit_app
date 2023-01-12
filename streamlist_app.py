9b6df4abb616cc86a7e6e6b4c9c0df2b6bcfb508
tracysnowpro <122344839+tracysnowpro@users.noreply.github.com>
2023-01-12 13:02:06 +0800

    Added calls to snowflake

--- streamlist_app.py
+++ streamlist_app.py
@@ -33,5 +33,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 
 # create a normalised version of the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 # present the normalised data in a table.
 streamlit.dataframe(fruityvice_normalized)
