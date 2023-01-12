df34ead6284143d0bad098b371110b09ee26362a
tracysnowpro <122344839+tracysnowpro@users.noreply.github.com>
2023-01-12 10:32:35 +0800

    Normalised and Tablicised the fruitvice data

--- streamlist_app.py
+++ streamlist_app.py
@@ -28,3 +28,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# create a normalised version of the data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# present the normalised data in a table.
+streamlit.dataframe(fruityvice_normalized)
