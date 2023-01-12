4c4a6d514efd58ec20cf264e5aa596c6a2a57e7c
tracysnowpro <122344839+tracysnowpro@users.noreply.github.com>
2023-01-12 13:15:39 +0800

    error

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,8 +1,8 @@
 
 import streamlit
 import pandas
-import snowflake.connector
 import requests
+import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Mom''s New Healthy Diner')
@@ -29,9 +29,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
 # create a normalised version of the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
